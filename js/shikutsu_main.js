f24f4dc2ba354afd207231f9bfcf09292780e07c
taka-github1 <the_dark_profound@yahoo.co.jp>
2023-04-25 10:10:01 +0900

    ok

--- js/shikutsu_main.js
+++ js/shikutsu_main.js
@@ -1175,7 +1175,7 @@ require([
               // console.log("files[" + i + "](read_file.then())");
               // 読み込みBlobデータ保持
               // var blob = args.event.target.result;
-              var blob = new Uint8Array(args.event.target.result);
+              var blob = new Uint32Array(args.event.target.result);
 
               $tr.find(".status").html("<span style='color:blue;'>登録中</span>");
               // アップロードファイル登録用Ajaxパラメータ作成
